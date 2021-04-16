e37adfdadbb918ce4666ce529f2638d1da6eb372
andy <admazzola@gmail.com>
2021-04-16 01:47:40 -0400

    link

--- src/views/Define.vue
+++ src/views/Define.vue
@@ -157,7 +157,7 @@
                  <div class="  p-4">
                     <div class="m-2 p-2 bg-gray-200 text-blue-800 inline-block"> This NFT definition can be used, by anyone, to mint the NFT on-chain. It is digitally signed by you as the artist.  </div> 
                     <br> 
-                     <router-link to="/mint" class="m-2 select-none no-underline text-white bg-green-500 p-2 inline-block rounded border-black border-2 cursor-pointer"> Mint the NFT </router-link >
+                     <a v-bind:href="getLinkToVirtualNFT()" class="m-2 select-none no-underline text-white bg-green-500 p-2 inline-block rounded border-black border-2 cursor-pointer"> Mint the NFT </a >
                      <a v-bind:href="getLinkToVirtualNFT()" target="_blank" class="m-2 select-none no-underline text-black bg-yellow-300 p-2 inline-block rounded border-black border-2 cursor-pointer">  Virtual NFT Page (share) </a >
            </div>
 
