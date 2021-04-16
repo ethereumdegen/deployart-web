f7d454c4af7c4544d1ff7929f30cf3464da3b04b
andy <admazzola@gmail.com>
2021-04-16 11:26:26 -0400

    footer

--- src/views/components/Footer.vue
+++ src/views/components/Footer.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="section py-4 text-white h-full" style="background: #1d1e26; min-height: 250px;">
     <div class=" h-full ">
-      <div class=" flex flex-row h-full">
-         <div class="w-1/4 ">
+       <div class=" flex lg:flex-row h-full">
+         <div class="lg:w-1/4 ">
          </div>
-        <div class="w-1/4 ">
+         <div class="w-1/2 lg:w-1/4 flex flex-col">
           <h3 class=" text-lg font-bold ">Documentation</h3>
 
 
           <a href="https://docs.deploy.art" target="_blank" class="no-underline text-sm text-gray-200 ">Deploy.Art Docs</a>
           </div>
-        <div class="w-1/4 h-full">
+         <div class="w-1/2 lg:w-1/4 flex flex-col">
           <h3 class=" text-lg font-bold ">Community &amp; Social</h3>
          
              <a href="https://guild.0xbtc.io" target="_blank" class="no-underline text-sm text-gray-200 ">Miners Guild</a>
