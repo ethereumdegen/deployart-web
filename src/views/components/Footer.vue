1d4c0a6e4a071708321160a2896f37caecaf697b
andy <admazzola@gmail.com>
2021-04-18 05:15:48 -0400

    purple

--- src/views/components/Footer.vue
+++ src/views/components/Footer.vue
@@ -8,15 +8,15 @@
           <h3 class=" text-lg font-bold ">Documentation</h3>
 
 
-          <a href="https://docs.deploy.art" target="_blank" class="no-underline text-sm text-gray-200 ">Deploy.Art Docs</a>
-           <a href="https://etherscan.io/address/0x4e3c5d37ca4d92b54248c411b496a0c959bcfe57" target="_blank" class="no-underline text-sm text-gray-200 ">Smart Contract</a>
+          <a href="https://docs.deploy.art" target="_blank" class="no-underline text-sm text-gray-200 hover:text-purple-500 ">Deploy.Art Docs</a>
+           <a href="https://etherscan.io/address/0x4e3c5d37ca4d92b54248c411b496a0c959bcfe57" target="_blank" class="no-underline text-sm text-gray-200 hover:text-purple-500">Smart Contract</a>
           
             
           </div>
          <div class="w-1/2 lg:w-1/4 flex flex-col">
           <h3 class=" text-lg font-bold ">Community &amp; Social</h3>
          
-             <a href="https://guild.0xbtc.io" target="_blank" class="no-underline text-sm text-gray-200 ">Miners Guild</a>
+             <a href="https://guild.0xbtc.io" target="_blank" class="no-underline text-sm text-gray-200 hover:text-purple-500">Miners Guild</a>
          </div>
 
 
